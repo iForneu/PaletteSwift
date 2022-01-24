53918a5d7acf07a3573bdaa050e6ec1a2343231d
iForneu <ikorotaevkirill@gmail.com>
2022-01-24 13:45:20 +0300

    Initial Commit

---
+++ PaletteSwift/PaletteSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteSwiftApp.swift
+//  PaletteSwift
+//
+//  Created by Kirill Korotaev on 24.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct PaletteSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
